eebe7d987cf482319fca1b96470386434f52a61a
Jeremy Browne <23028887+jeremy-browne@users.noreply.github.com>
2022-11-29 07:57:03 +1100

    Added styles

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ let items = [];
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     let today = new Date();
